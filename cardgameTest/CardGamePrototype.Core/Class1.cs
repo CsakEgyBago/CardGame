bacee90c7bfe39d02142eb94a8e2d9400db21b25
Joe Biden <bagoadamadrian@gmail.com>
2026-06-07 19:57:16 +0200

    delete useless class

--- cardgameTest/CardGamePrototype.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CardGamePrototype.Core;
-
-public class Class1
-{
-
-}
--- cardgameTest/CardGamePrototype.Core/obj/Debug/net10.0/CardGamePrototype.Core.AssemblyInfo.cs
+++ cardgameTest/CardGamePrototype.Core/obj/Debug/net10.0/CardGamePrototype.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardGamePrototype.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2f0ab6d1176c6536a38c1fdfbff98af3133b0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f3e7134914c665977e85e142c352128405b121")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardGamePrototype.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardGamePrototype.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
