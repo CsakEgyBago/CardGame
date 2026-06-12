39f561c57f749a5506332b8d3cb5eceb43487ae6
Joe Biden <bagoadamadrian@gmail.com>
2026-06-12 17:25:58 +0200

    good stuff

--- cardgameTest/CardGamePrototype.Client/obj/Debug/net10.0/CardGamePrototype.Client.AssemblyInfo.cs
+++ cardgameTest/CardGamePrototype.Client/obj/Debug/net10.0/CardGamePrototype.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardGamePrototype.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9485a26f27b875f2a39f724d6c5ec17c89125a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03ba219f7ae8ae9caf3221e870f8ca9af0dcf7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardGamePrototype.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardGamePrototype.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
